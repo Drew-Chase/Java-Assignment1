4b7945c21dac0423b194dc30eaea3baffcfb1901
Drew Chase <dcmanproductions@gmail.com>
2018-09-07 11:14:28 -0400

    dddd

---
+++ src/tk/dccraft/Assignment_1/part_1/Food.java
@@ -0,0 +1,39 @@
+package tk.dccraft.Assignment_1.part_1;
+
+import java.util.AbstractMap;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Map.Entry;
+
+public class Food {
+
+	private double price = 0;
+	private String type, name;
+
+	public Food(double price, String type, String name) {
+//		this.price = price;
+//		this.type = type;
+//		this.name = name;
+//
+//		if (type.toLowerCase() == "app".toLowerCase()) {
+//			MealTester.app = new HashMap<String, Double>();
+//			MealTester.app.put(name, price);
+//			MealTester.menu = new HashMap<String, Double>();
+//			MealTester.menu.put(name, price);
+//			System.out.println("Added Apps " + name + " = "+price);
+//		} else if (type.toLowerCase() == "Entrees".toLowerCase()) {
+//			MealTester.ent = new HashMap<String, Double>();
+//			MealTester.ent.put(name, price);
+//			MealTester.menu = new HashMap<String, Double>();
+//			MealTester.menu.put(name, price);
+//		} else if (type.toLowerCase() == "Beverages".toLowerCase()) {
+//			MealTester.bev = new HashMap<String, Double>();
+//			MealTester.bev.put(name, price);
+//			MealTester.menu = new HashMap<String, Double>();
+//			MealTester.menu.put(name, price);
+//		} else {
+//			System.out.println("Something Went Wrong with the Type\nIt should say \"app\", \"entrees\", or \"beverages\"\n It says: " + type);
+//		}
+	}
+
+}
---
+++ src/tk/dccraft/Assignment_1/part_1/MealTester.java
@@ -0,0 +1,113 @@
+package tk.dccraft.Assignment_1.part_1;
+
+import java.awt.Color;
+import java.awt.Dimension;
+import java.util.HashMap;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JTextArea;
+
+public class MealTester {
+
+	private String title = "Bennigan's POS System";
+	// private double price;
+	// private Food brBites, loPS, mozzStick, cCaP, steak, salmon, liver, cola,
+	// iceTea, lemonade, water;
+
+	public static HashMap<String, Double> menu = new HashMap<String, Double>();
+	public JFrame consoleWindow, orderWindow;
+	public JTextArea console;
+
+	public static void main(String[] args) {
+		new MealTester();
+	}
+
+	public MealTester() {
+		initConsoleWindow();
+		initApp();
+		initEnt();
+		initBev();
+		initOrderWindow();
+
+	}
+
+	public void initApp() {
+		print("---------- Initializing Appetizers ----------");
+		// brBites = new Food(5.39, "app", "Broccoli Bites");
+		menu.put("Broccoli Bites", 5.39);
+		print("Initializing Broccoli Bites");
+		// loPS = new Food(6.49, "app", "Loaded Potato Skins");
+		menu.put("Loaded Potato Skins", 6.49);
+		print("Initializing Loaded Potato Skins");
+		// mozzStick = new Food(5.29, "app", "Mozzarella Sticks");
+		menu.put("Mozzarella Sticks", 5.29);
+		print("Initializing Mozzarella Sticks");
+	}
+
+	private void initBev() {
+		print("--------- Initializing Beverages ------------");
+		menu.put("Coca-Cola", 3.00);
+		print("Initializing Coca-Cola");
+		menu.put("Iced Tea", 3.00);
+		print("Initializing Iced Tea");
+		menu.put("Lemonade", 3.00);
+		print("Initializing Lemonade");
+		menu.put("Tap Water", 2.00);
+		print("Initializing Tap Water");
+
+	}
+
+	private void initEnt() {
+		print("--------- Initializing Entrees --------------");
+
+	}
+
+	// Initializes Windows
+	private void initOrderWindow() {
+		// Initializing Frame
+		orderWindow = new JFrame(title + " -- Menu");
+		orderWindow.setSize(new Dimension(800, 600));
+		orderWindow.setVisible(true);
+		orderWindow.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		orderWindow.setLocationRelativeTo(null);
+
+		// Initializing UI
+		JButton button[] = new JButton[menu.size()];
+		print(menu.size() + " menu string length");
+		for (String s : menu.keySet()) {
+			for (double d : menu.values()) {
+				button[menu.size()] = new JButton(new String(s + " = " + d));
+				button[menu.size()].setSize(new Dimension(100, 50));
+				print("Added UI Element: " + s + " Button");
+			}
+		}
+		for (int i = 0; i < menu.size(); i++) {
+			orderWindow.add(button[i]);
+		}
+
+	}
+
+	public void initConsoleWindow() {
+		// Initializing Frame
+		consoleWindow = new JFrame(title + " -- Console");
+		consoleWindow.setSize(new Dimension(800 / 2, 600 / 2));
+		consoleWindow.setVisible(true);
+		consoleWindow.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		// consoleWindow.setResizable(false);
+
+		// Initializing UI
+		console = new JTextArea();
+		console.setEditable(false);
+		console.setSize(new Dimension(consoleWindow.getWidth() - 50, consoleWindow.getHeight() - 50));
+		console.setBackground(Color.BLACK);
+		console.setForeground(Color.WHITE);
+		consoleWindow.add(console);
+	}
+
+	public void print(String message) {
+		System.out.println(message);
+		console.append(message + "\n");
+	}
+
+}
