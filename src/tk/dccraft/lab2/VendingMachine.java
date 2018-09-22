40c32e51d95fa09cd3237e52ea086e2c3cacaefb
Drew Chase <dcmanproductions@gmail.com>
2018-09-22 15:57:18 -0400

    working on lab2

---
+++ src/tk/dccraft/lab2/VendingMachine.java
@@ -0,0 +1,29 @@
+package tk.dccraft.lab2
+
+public class VendingMachine extends Main{
+	private int canAmount = 0, maxCanAmount = 25, cashAmount = 50;
+	
+	public void fill(){
+		for(int i = canAmount; i < maxCanAmount; i++){
+			canAmount++;
+			print("Added one can to the vending machine\nVending Machine has " + canAmount+ " in it currently");
+		}
+		print("Vending Machine is full");
+	}
+	
+	public void dispense(){
+		canAmount--;
+	}
+	
+	public int getBills(){
+		return cashAmount;
+	}
+	
+	public int getCanCount(){
+		return canAmount;
+	}
+	
+	public VendingMachine(int canAmount){
+		this.canAmount = canAmount;
+	}
+}
